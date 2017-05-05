9b76bf4ba23634af229e02b2192332302094462f
Tanner Eustice <teustice@msn.com>
2017-05-05 09:10:15 -0700

    add method to project that saves a new project

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,12 @@
+require 'securerandom'
+
 module Project
   def self.all
     DB.exec("SELECT * FROM projects").to_a
+  end
+
+  def self.save(name, description)
+    id = SecureRandom.uuid
+    DB.exec("INSERT INTO projects (id, name, description) VALUES ('#{id}','#{name}','#{description}') RETURNING id")[0]['id']
   end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,4 +6,15 @@ describe 'Project' do
       expect(Project.all).to eq []
     end
   end
+
+  describe '.save' do
+    it 'It will save a Project to the projects table' do
+      name = "Litter clean up"
+      description = "Clean up litter around Portland"
+      project_id = Project.save(name, description)
+      expect(Project.all[0]["id"]).to eq project_id
+      expect(Project.all[0]["name"]).to eq name
+      expect(Project.all[0]["description"]).to eq description
+    end
+  end
 end
